4d590b12639fd29aa542121e516ab43705fa85b3
Lyle-Lyle <xiongjinyue.lyle@gmail.com>
2023-06-16 15:06:38 -0400

    render nab bar

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,16 @@
+// require('@rushstack/eslint-patch/modern-module-resolution')
 /* eslint-env node */
 module.exports = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    // '@vue/eslint-config-typescript'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 0,
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
+  <LayoutFooter />
 </template>
 
-<style scoped>
+
+<style scoped lang="scss">
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -83,3 +71,4 @@ nav a:first-of-type {
   }
 }
 </style>
+
